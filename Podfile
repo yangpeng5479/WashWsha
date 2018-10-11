152ba556747176b379eed2da513e59efa454ed22
roc <roc@italkly.com>
2018-10-11 18:54:44 +0800

    4

---
+++ Podfile
@@ -0,0 +1,25 @@
+platform :ios,"8.0"
+target "Vcoze" do
+pod "MJRefresh"
+pod "SDWebImage"
+pod "AFNetworking"
+pod "MJExtension"
+pod "Masonry"
+pod "TYPagerController"
+pod "SDCycleScrollView",'~> 1.75'
+pod "FBSDKLoginKit"
+pod "pop"
+pod "MBProgressHUD"
+pod "MarqueeLabel"
+pod "TYAttributedLabel"
+pod "RegexKitLite"
+pod "UMengAnalytics-NO-IDFA"
+pod "JPush"
+pod "mob_smssdk"
+pod "MMDrawerController"
+pod "SVGAPlayer"
+pod "ZBLiveBarrage"
+pod "mob_sharesdk/ShareSDKPlatforms/Facebook"
+pod "AgoraRtcEngine_iOS"
+
+end
---
+++ Talk.lyTests/Talk_lyTests.m
@@ -0,0 +1,39 @@
+//
+//  Talk_lyTests.m
+//  Talk.lyTests
+//
+//  Created by 杨鹏 on 2018/2/6.
+//  Copyright © 2018年 Talk.ly. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Talk_lyTests : XCTestCase
+
+@end
+
+@implementation Talk_lyTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
